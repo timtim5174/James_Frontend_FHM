673726e8986d9b4e03ec108a13e1d7ed7c72f28d
Lutz Steinkamp <lutz.steinkamp92@gmail.com>
2018-05-12 12:58:28 +0200

    Created a simple project-structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignOutComponent } from './components/sign-out/sign-out.component';
+import { EqualPasswordsDirective } from './components/sign-up/equal-passwords.directive';
+import { EmailDirective } from './shared/authentication/email.directive';
+import { PasswordDirective } from './shared/authentication/password.directive';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    SignOutComponent,
+    EqualPasswordsDirective,
+    EmailDirective,
+    PasswordDirective,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/sign-up/equal-passwords.directive.spec.ts
@@ -0,0 +1,8 @@
+import { EqualPasswordsDirective } from './equal-passwords.directive';
+
+describe('EqualPasswordsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new EqualPasswordsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
