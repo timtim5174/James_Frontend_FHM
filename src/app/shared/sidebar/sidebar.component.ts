2f04947d4dd07f480feba415c4fd1e57af26ac9b
Till Hoffmann <th274406@fh-muenster.de>
2018-05-24 14:31:24 +0200

    Created animated Sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, style, animate, transition, state } from '@angular/animations';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  animations: [
+    trigger('toggleState', [
+      // What happens when toggleState is true
+      state('true', style({
+        left: '0px',
+      })),
+      // What happens when toggleState is false
+      state('false', style({
+        left: '-160px',
+      })),
+      // transition
+      transition('* => *', animate('300ms')),
+    ]),
+  ],
 })
 export class SidebarComponent implements OnInit {
+  @Input() shouldToggle = true;
 
   constructor() {
   }
 
   ngOnInit() {
   }
-
 }
 
