0ede0cdcc9e6e2b5912478db2c0315f0617c28c7
vf691184 <vf691184@fh-muenster.de>
2018-06-06 17:42:57 +0200

    updated bookInfoGraph

--- src/app/components/transaction/shared-transaction.service.ts
+++ src/app/components/transaction/shared-transaction.service.ts
@@ -12,11 +12,12 @@ export class SharedTransactionService {
   constructor() { }
 
   setTransactions(transactions: Transaction[]) {
+    // tslint:disable-next-line:max-line-length
+    transactions = transactions.sort(((a: Transaction, b: Transaction) => (new Date(a.creationDate).getTime()) - (new Date(b.creationDate).getTime())));
     this.transactions.next(transactions);
   }
 
   getTransactions(): Observable<Transaction[]> {
     return this.transactions.asObservable();
   }
-  
 }
