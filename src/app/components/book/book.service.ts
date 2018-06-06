1dcbf6d03cc694fd69979bd51b3d461f081b99bc
Lutz Steinkamp <lutz.steinkamp92@gmail.com>
2018-06-06 19:42:22 +0200

    Updated book.service --> sort-function works now

--- src/app/components/book/book.service.ts
+++ src/app/components/book/book.service.ts
@@ -18,10 +18,19 @@ export class BookService {
 
   getBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(this.baseURL + '/getBooks').pipe(
-      map(books => books.sort(((a: Book, b: Book) => a.title <= b.title ? 0 : 1))),
+      map(books => books.sort(((a: Book, b: Book) => {
+        if (a.title > b.title) {
+          return 1;
+        } else if (a.title < b.title) {
+          return -1;
+        } else {
+          return 0;
+        }
+      }))),
       catchError(this.handleError)
     );
   }
+
 
   createBook(book: Partial<Book>): Observable<Book> {
     return this.http.post<Book>(this.baseURL + '/createBook', book).pipe(
