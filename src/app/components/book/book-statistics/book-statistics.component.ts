9d78146e36f516f5aac286e5794a084291c53540
vf691184 <vf691184@fh-muenster.de>
2018-06-14 13:19:38 +0200

    bug fixing

--- src/app/components/book/book-statistics/book-statistics.component.ts
+++ src/app/components/book/book-statistics/book-statistics.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PieGraph } from '../../../shared/graphs/pie-graph/pie-graph';
+import { BarGraph } from '../../../shared/graphs/bar-graph/bar-graph';
+import { SharedBookService } from '../shared-book.service';
+import { SharedTransactionService } from '../../transaction/shared-transaction.service';
+import { Book } from '../book';
+import { Transaction } from '../../transaction/transaction';
 
 @Component({
   selector: 'app-book-statistics',
@@ -7,10 +12,49 @@ import { PieGraph } from '../../../shared/graphs/pie-graph/pie-graph';
   styleUrls: ['./book-statistics.component.scss']
 })
 export class BookStatisticsComponent implements OnInit {
-  dataPieChart: PieGraph;
+  dataBarGraph: BarGraph;
+  book: Book;
+  transactions: Transaction[];
 
-  constructor() { }
+  constructor(private sharedBookService: SharedBookService, private sharedTransactionService: SharedTransactionService) { }
 
   ngOnInit() {
+    this.sharedBookService.getBookData().subscribe(book => {
+      if (book !== null && book !== this.book) {
+        this.book = book;
+      }
+      this.sharedTransactionService.getTransactions().subscribe(transactions => {
+        if (transactions !== null && transactions !== this.transactions) {
+          this.transactions = transactions;
+          console.log(this.book);
+          console.log(this.transactions);
+          this.barGraph();
+        }
+      });
+    });
+  }
+  barGraph() {
+    this.dataBarGraph = {
+      type: 'bar',
+      data: {
+        labels: ['test1', 'test2', 'test3', 'test4', 'test5'],
+        datasets: [
+          {
+            label: 'Einkunft',
+            backgroundColor: ['rgba(23,162,184)'],
+            data: [-100, 200, 300, 400, 500]
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: true
+        },
+        title: {
+          display: true
+        }
+      }
+    };
   }
 }
+
