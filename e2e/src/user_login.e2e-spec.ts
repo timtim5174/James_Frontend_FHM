8ea092e0e01c5ca3cbd6dbfba8464f4636f11db6
Tim Sawatzki <tsawatzki@fh-muenster.de>
2018-06-15 09:38:50 +0200

    updated user login e2e test

--- e2e/src/user_login.e2e-spec.ts
+++ e2e/src/user_login.e2e-spec.ts
@@ -3,29 +3,24 @@ import { browser, by, element, Key, WebElement } from 'protractor';
 describe('User login test', () => {
     browser.waitForAngularEnabled(false);
 
-    beforeEach(async() => {
+    beforeAll(async() => {
         await browser.get('http:/localhost:4200');
         browser.manage().window().maximize();
+        await element(by.name('login')).click();
+        await element(by.name('email')).sendKeys('tim@test.de', Key.RETURN);
     });
 
     // Login
     it('should not possible to login user -> password wrong', async() => {
-        await element(by.name('login')).click();
-        const email: WebElement = element(by.name('email'));
-        const password: WebElement = element(by.name('password'));
-        email.sendKeys('tim@test.de', Key.RETURN);
-        password.sendKeys('test123456', Key.RETURN);
+        await element(by.name('password')).sendKeys('test123456', Key.RETURN);
         await browser.sleep(3000);
         const alert = await element(by.className('alert')).isPresent();
         expect(alert).toBe(true);
     });
 
     it('should possible to login user', async() => {
-        await element(by.name('login')).click();
-        const email: WebElement = element(by.name('email'));
-        const password: WebElement = element(by.name('password'));
-        email.sendKeys('tim@test.de', Key.RETURN);
-        password.sendKeys('test1234', Key.RETURN);
+        await element(by.name('password')).clear();
+        await element(by.name('password')).sendKeys('test1234', Key.RETURN);
         await browser.sleep(3000);
         const alert = await element(by.className('alert')).isPresent();
         expect(alert).toBe(false);
