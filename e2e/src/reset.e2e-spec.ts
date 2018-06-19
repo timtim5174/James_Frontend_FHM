18ebd95feafa79805a9021a535bf3813ec0ca093
Tim Sawatzki <tsawatzki@fh-muenster.de>
2018-06-19 17:13:06 +0200

    Added e2e tests, removed app e2e from angular

--- e2e/src/reset.e2e-spec.ts
+++ e2e/src/reset.e2e-spec.ts
@@ -3,9 +3,16 @@ import { browser, by, element} from 'protractor';
 describe('Reset created test data', () => {
     browser.waitForAngularEnabled(false);
 
-    // transaction
+    // delete created transaction
+    it('should delete created transaction', async() => {
+        await element(by.className('fa-trash')).click();
+        await element(by.className('btn-danger')).click();
+        await browser.sleep(2000);
+        const result = await element(by.className('modal-title')).isPresent();
+        expect(result).toBe(false);
+    });
 
-    // book
+    // delete created book
     it('should delete created book', async() => {
         await browser.actions().mouseMove(element(by.className('close'))).perform();
         await element(by.className('close')).click();
@@ -16,7 +23,7 @@ describe('Reset created test data', () => {
         expect(result).toBe(false);
     });
 
-    // user
+    // delete created user account
     it('should delete user account', async() => {
         await element(by.name('myAccount')).click();
         await element(by.id('deleteAccount')).click();
