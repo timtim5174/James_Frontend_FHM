2485a273bd5f20afc09169274849ceae882ddbaf
Tim Sawatzki <tsawatzki@fh-muenster.de>
2018-06-23 15:39:43 +0200

    Modifed james.conf.ts to change server connection

--- src/james.conf.ts
+++ src/james.conf.ts
@@ -1,3 +1,11 @@
 export default{
-    restApiUrl: 'http://localhost:8080/JamesBackend-web/api/v1/boarding'
+    restApiUrl: serverConnection('L')
 };
+
+function serverConnection(type: 'L' | 'P' | 'T') {
+    switch (type) {
+        case 'L': return 'http://localhost:8080/JamesBackend-web/api/v1/boarding'; // local
+        case 'P': return 'http://10.60.64.72:80/JamesBackend-web/api/v1/boarding'; // produtkion
+        case 'T': return 'http://10.60.64.72:8080/JamesBackend-web/api/v1/boarding'; // test
+    }
+}
